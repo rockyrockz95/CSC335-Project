904ad0f91511b7ff44601a5c9b8544c49bf105b2
Joquanna <62509419+rockyrockz95@users.noreply.github.com>
2025-05-13 20:02:06 -0400

    Removed let discussion and Refined Specification of System

--- meeting2/1.3.scm
+++ meeting2/1.3.scm
@@ -2,75 +2,18 @@
 
 ;;; 1.3 Environment Subsystem Proof & Change in Representation
 
-;====================================================================
-#|
- Environment Specification for TLS:
-  ;; The environments of TLS are composed of ([name][value]) pair entries.
-     ;; Functions that makeup the environment subsystem:
-;         - new-entry
-;         - lookup-in-entry
-;         - extend-table
-;         - lookup-in-table
-;         - initial-table
-|#
-;======================================================================
-
-;;;; TLS-Let: helper for subsystem proof
-       ; Since lambda creates closures and keeps track of its env, it can be used to display
-       ;   environment changes as we change contours
-;;; let structure:  (let <(<var><exp>)...)> <body>)
- ;; let == syntactic sugar for lambda: (let ((name val)) exp) == ((lambda (name) exp) val)
-     ; To be used in tests to ensure that *let works as syntactic sugar for lambda
-
-; Accessors
-(define *let-body caddr)
-
-; Pre: Given a let expr
-; Post: Return the variables from the bindings
-(define (*let-var expr)
-  (let loop ((binds (*let-bindings expr)))
-    (cond ((null? binds) '())
-          (else (cons (caar binds) (loop (cdr binds)))))))
-
-; Pre: Given a let expr
-; Post: Return the init values from the bindings
-(define (*let-init expr)
-  (let loop ((binds (cadr expr)))
-    (cond ((null? binds) '())
-          (else (cons (cadr (car binds)) (loop (cdr binds)))))))
-
-; Predicate
-(define (*let? expr)
-   (eq? (car expr) '*let))
-
-;;; Action Function -- Handles *let expresseions in TLS
- ;; IDEA: (i) let should evaluate each of its args before the body --- correctness standard acc. documentation
- ;;       (ii) should keep track of the current env like lambda implementation
- ;;       *** (i) can't garuntee order of operations
-; Pre: Given a let expr, e, and the current env, table
-; Post: Evaluates the let expression and stores its current environment
-
-(define (*let expr table)
-  (let ((var (*let-vars expr))
-        (init (*let-init expr)))
-    ; evaluate and store init values
-    (meaning init (cons table '()))
-    ; extend table with evaluated body
-    (meaning (*let-body expr) (cons (new-entry var (evlis init table))
-                                    table))))
-
-; Test Cases
-;;; ((lambda (x) x) 'hello) == (let ((x 'hello)) x)
-;(*let ((x 'hello) x))
-;(value '(*let ((x 'hello)) x))
-
-
-;===========================================================================================
+;=======================================================================================================================
 #|
  Environment Specification for TLS:
   ;; TLS' environment subsystem manages the ((name)(value)) pair entries that makeup the environment.
-     
-  ;; The primary operations that handle environment management are:
+
+  ;; Environment properties: ****
+      - Env structure: A nested list
+      - Entry structure: "An entry is a pair of lists whose first list is a set. Also,
+          the lists must be of equal length."
+            * Implies that every valid variable in TLS is bound
+  
+  ;; The primary operations that handle environment management are: 
          - new-entry:
              * Pre-condition: The first argument is a list of names and the second is a list of corresponding values.
              * Post-condition: Returns a new environment entry, represented as a pair of names and values.
@@ -83,34 +26,73 @@
          - lookup-in-table
              * Pre-condition: table is a list of environment entries.
              * Post-condition: If the name is found in any entry, return its value; otherwise, invoke table-f.
-
-  ;; In tandem, these functions are used to build and maintain a stack of environment entries (bindings).
-
-  ;; Interpreter property:
-      - Lexical Scoping: In the environment level*, maintained by extend-table.
-         * As opposed to the underlying R5RS
   
-  ;; Environment properties:
-      - Env structure: A nested list
-      - Entry structure: "An entry is a pair of lists whose first list is a set. Also,
-          the lists must be of equal length."
+  ;; In tandem, these functions are used to build and maintain a stack of environment entries (bindings).
+  
+  ;; Interpreter objects that use the subsystem:
+      - *identifier: uses lookup-in-table to create var references
+      - *lamdba: creates a closure within the environment
+      - Other action functions: don't modify the table, but keep track of it 
+      - apply-closure: evaluates the body of the function and extends the existing environment
+          with its arguments
+  
+  ;; All of these functions keep track of and/or use the environment maintain the value of bound variables
+  
+  ;; Interpreter property:
+      - Lexical Scoping: In the environment level*, maintained by extend-table. Cons'ing the new entries to the front to the stack is analogous to
+                  going deeper into scope frames as new closures are created. 
+             - In the action functions that modify the table, lookup-in-table is used to provide the first occurrence of the variable in the table
+                 As established before, new-entry adds entries to the top of the stack. By retrieving the first occurence
+                 of the variable in the stack, the action functions are using the value assigned in the latest frame of the
+                  environment. Thus, lexical scope is maintained by this interpreter.
+         
+             * As opposed to the underlying R5RS
+
+  ;; Claim: Our implementation* satisfies this specification as well.
+    * Will be calling TLS-335 for clarity
+
+    - Primary environment operations: Our interpreter makes no changes to the original TLS implementation
+        therefore it is true that in the case of lookup and adding variables, TLS and TLS-335 function the same.
+
+    - Lexical scoping:
+        - Each new entry is added to the top of the stack ~= nesting lambda statements create deeper
+              closures that hold free variables and their values specific to their body.
+        - The only new operation dealing with closures in our system is the syntax checker, which deals with
+              * lambda
+              * unbound vars
+              - In either case, the checker only verifies errors in syntax using an internal table. Since
+                  the interpreter's table is not changed, its handling of closures should be maintained.
                      
 |#
-;======================================================================================
+;==========================================================================================================================
 
 
 
-#| ======================================================================================
+#| ========================================================================================================================
    Part 2: Change the representation of the env and show that the representation satisfies the spec
      and works with the rest of the interpreter
+
        Claim: The environment subsytem of TLS is structured in a way that allows it to act as
-         an abstract data type. Therefore, an a-list structure for the env still satisfies
-         the specifications stated before.
+         an abstract data type. Therefore, an a-list structure for the env, rather than the list of bindings, still satisfies
+         the specifications stated above.
 
-       ; Assume that the input is already in a-list form: ((name value) (name2 value2) ....)
+       ; Environment properties: The only functions that need to be changed to adjust to the new
+          structure are the the primary components of the subsystem.
+       
+       ; Interpreter properties: The implementation of the action functions do not need to change. As the ADS allows for
+          operations to work even with differing structure
+         
 
-   ============================================================================================
+   ========================================================================================================================
 |# 
+
+;;; Environment Changes
+  ;; Creating an env: build & new-entry
+  ;; Extending the table -- needs no change, cons'ing the pair to the front of the stack maintains scope
+    ; and fulfills the expectations of the action functions
+  ;; Table Lookup: lookup-in-entry & lookup-in-table -- must be changed to account for alist structure.
+
+;;; Action Function Calls ****
 
 
   
