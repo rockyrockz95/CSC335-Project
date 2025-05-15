92417c0c4be4e9dad34ef628f93877cdef5b04f0
LingJie Pan <166444602+ling027@users.noreply.github.com>
2025-05-15 00:07:28 -0400

    Update PostMeetingSyntax_V2.rkt

--- PostMeetingSyntax_V2.rkt
+++ PostMeetingSyntax_V2.rkt
@@ -1,31 +1,35 @@
 (load "1.1.rkt")
 
 ;;Top Level Function
+
 ;pre: expression
 ;post: return ture if the exp is a valid expression, else return #f
 
-;Base Case: If the exp is null, then it is valid as empty list is a valid expression
-;If the current element of exp is a pari, then it should check whether the pari is a lambda expression cond expression or primitives, if yes, then it should check the exp using the lambda-checker/cond-checker/primitive-length, and since we sitll need to care about the correctness of the rest of the expression, we need to also check the syntax of the rest of the expression.
-;if the element if not a pari, then we should check if it is a primitive, if it is then we should check whether it has the correct length, also the correctness of the rest of the expression
-;if none of the clauses above were triggered, then it should recurse into the expression and check the rest of the expression.
+;;Wrapper function
+;;Check whether the exp is an atom, if it is, then it needs to be a rational number, or symbol, or a boolean. If it is none of the above, then it should return #f
+;;If the exp is a pair, then it should call the syntax-checker to check the expression exp.
 
 (define (simple-check exp)
   (cond ((null? exp) #t)
         ((atom? exp)
           (or (or (rational? exp) (symbol? exp)) (boolean? exp)))
-        ((pair? exp) (syntax-checker exp '())) ; empty env to add closure records -- placeholder now
+        ((pair? exp) (syntax-checker exp '()))
         ))
 
-; added env to deep search
-  ; lambda creates closures + extends table
-  ; cond should not unless lambda is nested within
-  ; free-var check should have access to entries and their values
+;pre: expression
+;post: return ture if the exp is a valid expression, else return #f
+
+;Base Case: If the exp is null, then it is valid as empty list is a valid expression
+;If the current element of exp is a pair, then it should check whether the pair is a lambda expression cond expression or primitives, if yes, then it should check the exp using the lambda-checker/cond-checker/primitive-length, and since we sitll need to care about the correctness of the rest of the expression, we need to also check the syntax of the rest of the expression.
+;if the element if not a pair, then we should check if it is a lambda/cond/primitives, if it is then we should check the correctness of the expression, also the correctness of the rest of the expression
+;if none of the clauses above were triggered, then it should recurse into the expression and check the rest of the expression.
+
 (define (syntax-checker exp env)
   (cond ((null? exp) #t)
         ((pair? (car exp))
-         (cond ((eq? (caar exp) 'lambda) (and (lambda-checker (car exp)) (syntax-checker (cdr exp) (extend-table (cadr (car exp)) env)))) ;if the current subexpression is a lambda expression, then it should call lambda expression, and check the rest of the expression.
+         (cond ((eq? (caar exp) 'lambda) (and (lambda-checker (car exp)) (syntax-checker (cdr exp) (extend-table (cadr (car exp)) env)))) 
                ((eq? (caar exp) 'cond) (and (cond-checker (car exp)) (syntax-checker (cdr exp) env)))
-               ((member (caar exp) primitives) (and (primitive-length (car exp)) (syntax-checker (car exp) env)))
+               ((member (caar exp) primitives) (and (and (primitive-length (car exp)) (syntax-checker (car exp) env)) (syntax-checker (cdr exp) env)))
                (else (and (simple-check (car exp)) (simple-check (cdr exp))))))
         ((eq? (car exp) 'lambda) (and (lambda-checker exp) (simple-check (cdr exp))))
         ((eq? (car exp) 'cond) (and (cond-checker exp) (simple-check (cdr exp))))
@@ -33,7 +37,7 @@
         (else (and (simple-check (car exp)) (simple-check (cdr exp))))))
 
 ;define a list of primitives
-(define primitives '(+ - * cons car cdr cond lambda modulo define if and or > < = eq? null? zero?))
+(define primitives '(+ - * cons car cdr cond lambda modulo define if and or > < = eq? null? zero? / quotient))
 
 
 ;pre: expression
@@ -42,6 +46,8 @@
   (cond ((eq? (car exp) '+) (eq? (length exp) 3))
         ((eq? (car exp) '-) (eq? (length exp) 3))
         ((eq? (car exp) '*) (eq? (length exp) 3))
+        ((eq? (car exp) '/) (eq? (length exp) 3))
+        ((eq? (car exp) 'quotient) (eq? (length exp) 3))
         ((eq? (car exp) 'cons) (eq? (length exp) 3))
         ((eq? (car exp) 'lambda) (eq? (length exp) 3))
         ((eq? (car exp) 'modulo) (eq? (length exp) 3))
@@ -68,21 +74,32 @@
 ;;Pre: Expression
 ;;Post: Return #t if the expression is a valid cond expression, else #f
 
+;;For a cond expression, the expression needs to start with cond and have at least 1 clause. If it is then it should recurse into the clauses and check all the clauses, if it is not, then it should return #f
+;;Since we already check whether the cond expression has at least 1 clause at the beginning, when recurse into the clauses, if the clauses is null, then that means we have gone through all clauses without flagging #f, hence return #t
+;;Clauses need to be a pair, if not then return #f
+;;Each clause needs to be a pair, if not then return #f
+;;If it is a else-clause, then it should be the last clause of the expression, if not return #f. If it is the last clause, then we should syntax-check the result statement of the else clause.
+;;predicate statement of each clause cannot be an atom, if it is then it should return #f
+;;result statement of each clause needs to be the last element of a clause, if not then it should return #f
+;;if the predicate statement is a pair, then it should check whether the procedure is one of primitives, if it is then it should syntax check the predicate, result statement and the rest of the clauses. If any of them return false, then the expression should be invalid.
+;;each clause should have a predicate + result, which means the length of each clause can not be less than 2, if so, return #f
+;;if none of the above were triggered, then it should recurse into the rest of the expression and check whether it is valid.
+
 (define (cond-checker expr)
-  (if (and (eq? (car expr) 'cond) (not (null? (cdr expr)))) ;Check whether the expression is a cond expression and if it has at least 1 clause
+  (if (and (eq? (car expr) 'cond) (not (null? (cdr expr))))
       (let loop ((clauses (cdr expr))) ;declare a recursive loop and extract the clauses
-        (cond ((null? clauses) #t)     ;if the function was able to get to the end of the expressio without trigger any condition, then it should return #t
-              ((not (pair? clauses)) #f) ;if the clauses are nor pair then it is invalid expression, hence return #f
-              ((not (pair? (car clauses))) #f) ;each clause should be a pair, if not, then  the expression is invalid, hence return #f
+        (cond ((null? clauses) #t)     
+              ((not (pair? clauses)) #f) 
+              ((not (pair? (car clauses))) #f) 
               ((eq? (caar clauses) 'else)
-               (cond ((not (null? (cdr clauses))) #f) ;if it is a else clause, then it should be the last clause of the expression, if not then it should return #f,
-                     (else (simple-check (cdr (car clauses)))))) ;syntax check the result statement of else clause
-              ((atom? (caar clauses)) #f) ;predicate cannot be an atom, if it is an atom, then return #f
+               (cond ((not (null? (cdr clauses))) #f)
+                     (else (simple-check (cdr (car clauses)))))) 
+              ((atom? (caar clauses)) #f) 
+              ((not (null? (cddr (car clauses)))) #f) 
               ((pair? (caar clauses)) (if (member (caaar clauses) primitives) (and (and (simple-check (caar clauses)) (simple-check (cdr (car clauses)))) (loop (cdr clauses))) #f)) 
-              ((< (length (car clauses)) 2) #f) ;each clause should have a condition + result, which means the length of each clause can not be less than 2, if so, return #f
-              ;((pair? (cadr (car clauses))) (and (syntax-checker (cadr (car clauses))) (loop (cdr clauses)))) ;
-              (else  (loop (cdr clauses))))) ;if none of the clauses were triggered, then it should search the rest of the clauses
-      #f)) ;Occur when the expression did not start with cond or the cond expression has 0 clause.
+              ((< (length (car clauses)) 2) #f)
+              (else  (loop (cdr clauses))))) 
+      #f)) 
 
 
 ;;Test Cases
@@ -130,31 +147,50 @@
 ;;Target: Elements that we want to find
 ;;set: the list that we are searching
 
+;;Base case: if the list is null, then it should return #f
+;;If at any point, the current element is equal to the target value, then it should return #t
+;If not, then it should search through the next element
+
 (define (find? target set)             
-  (cond ((null? set) #f) ;Base case: if the list is null, then it should return #f
-        ((eq? (car set) target) #t) ;If at any point, the current element is equal to the target value, then it should return #t
-        (else (find? target (cdr set))))) ;If not, then it should search through the next element
+  (cond ((null? set) #f)
+        ((eq? (car set) target) #t) 
+        (else (find? target (cdr set))))) 
 
 ;;Helper function that help to determine whether a list contains duplicated element, in this case we need to determine whether the parameters are duplicated in the lambda expression
 ;;set: the list we are searching through
 
+;Pre: A list x
+;Post: return true if elements in the list x repeated, else return #f
+
+;;Base case: if the current element is the last element then it should return #f as it is the only element in the list, hence can not be repeated
+;If at any point, the current element is found again in the rest of the list, then that means the elements was repeated, hence return #t
+;If not, then it should search the rest of the list
+
 (define (repeated? set)                 
-  (cond ((null? (cdr set)) #f) ;Base case: if the current element is the last element then it should return #f as it is the only element in the list, hence can not be repeated
-        ((find? (car set) (cdr set)) #t) ;If at any point, the current element is found again in the rest of the list, then that means the elements was repeated, hence return #t
-        (else (repeated? (cdr set))))) ;If not, then it should search the rest of the list
+  (cond ((null? (cdr set)) #f)
+        ((find? (car set) (cdr set)) #t) 
+        (else (repeated? (cdr set))))) 
     
 ;;Pre: Expression
 ;;Post; Return #t if the expresion pass the checker, else #f
+
+;;For a valid lambda expression it needs to start with lambda and with a length of 3.So we need to check whether the expression starts with lambda if no, then its not lambda.
+;;Parameters of lambda cannot be atom, it has to be a pair, so if the parameter of the expression is atom, then it should return #f
+;;Parameters of lambda expression cannot repeated, it if repeated then the expression is not valid
+;;the body of the lambda expression needs to have something,if not, then it should return #f
+;;if the body of the lambda expression is a pair, then it should syntax check the body
+;;if none of the clauses were triggered, then it should return #t
+
 (define (lambda-checker expr)
-  (if (and (eq? (car expr) 'lambda) (eq? (length (cdr expr)) 2)) ;check whether the expression starts with lambda if no, then its not lambda. If yes, then it should contains a parameter list and a body
+  (if (and (eq? (car expr) 'lambda) (eq? (length (cdr expr)) 2)) 
       (let ((parameter (cadr expr))      ;extract the parameter list
             (body (caddr expr)))         ;extract the body 
-        (cond ((atom? parameter) #f)     ;if the paramter list is an atom, then its invalid
-              ((and (pair? parameter) (repeated? parameter)) #f)       ;if the parameters repeated, then its not valid
-              ((null? body) #f) ;if the body is null, then the expression is not valid
-              ((pair? body) (simple-check body)) ;if the body is a pair, the syntax check the body
-              (else #t))) ;if it triggered none of the above clauses, then it should return true                    
-      #f)) ;Occur when the expression did not start with lambda or it does not have exactly two parts: parameters and body
+        (cond ((atom? parameter) #f)     
+              ((and (pair? parameter) (repeated? parameter)) #f)     
+              ((null? body) #f)
+              ((pair? body) (simple-check body)) 
+              (else #t)))                    
+      #f)) 
 
 ;;Test Cases
 
@@ -197,8 +233,39 @@
 
 (simple-check '(define x (lambda (x) (+ x 1 2))));->f
 
-(simple-check '(define (module x) (define (add m n) (cond ((zero? m) n) (else (+ 1 (add (- m 1) n))))) (cond ((eq? m '+) add))));->t
+(simple-check '(define (module x) (define (add m n) (cond ((zero? m) n) (else (+ 1 (add (- m 1) n))))) (cond ((eq? m add) add))));->t
 
+(simple-check '(define (module x)
+                 (define (add m n) (cond ((zero? m) n) (else (+ 1 (add (- m 1) n)))))
+                 (define (mul m n) (cond ((zero? m) 0) (else (add n (add (- m 1) n)))))
+
+                 (cond ((eq? x add) add)
+                       ((eq? x mul) mul))));->t
+
+(simple-check '(define (module x)
+                 (define (add m n) (cond ((zero? m) n) (else (+ 1 (add (- m 1) n)))))
+                 (define (mul m n) (cond ((zero? m) 0) (else (add n (add (- m 1) n)))))
+
+                 (cond ((eq? x add) add)
+                       ((eq? x mul) mul 1))));->f
+
+(simple-check '(define (module x)
+                 (define (add m n) (cond ((zero? m) n) (else (+ 1 (add (- m 1) n)))))
+                 (define (mul m n) (cond ((zero? m) 0) (else (add n (add (- m 1) n)))))
+                 (define (sub m n) (cond ((zero? n) m) (else (- (sub m (- n 1)) 1))))
+
+                 (cond ((eq? x add) add)
+                       ((eq? x mul) mul)
+                       ((eq? x sub) sub))));->t
+
+(simple-check '(define (module x)
+                 (define (add m n) (cond ((zero? m) n) (else (+ 1 (add (- m 1) n)))))
+                 (define (mul m n) (cond ((zero? m) 0) (else (add n (add (- m 1) n)))))
+                 (define (sub m n) (cond ((zero? n) m) (else (- (sub m (- n 1 1)) 1))))
+
+                 (cond ((eq? x add) add)
+                       ((eq? x mul) mul)
+                       ((eq? x sub) sub))));->f
 
 
 #| ==========================================================================================
